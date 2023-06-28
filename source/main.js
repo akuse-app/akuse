5dc218ea257d4c102bb1fea390d4a8b81b8c63b2
aleganza <ganzarolli.alessio@gmail.com>
2023-06-28 14:37:40 +0200

    try n1 to add jquery

--- source/main.js
+++ source/main.js
@@ -6,9 +6,14 @@ const createWindow = () => {
     height: 720,
     // fullscreen: true,
     autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
   win.loadFile('source/index.html')
+  
+  /* $("#test").text("ciaooooo"); */
 }
 
 app.whenReady().then(() => {
