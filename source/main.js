1a8563c12e5b28bff5f0f027edf24beeb758c5e2
aleganza <ganzarolli.alessio@gmail.com>
2023-07-14 00:35:11 +0200

    add axios, access token is finally retrieved
    added axios package removed request package

--- source/main.js
+++ source/main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron")
 const path = require("path")
 const url = require('url')
-var request = require('request')
+const axios = require('axios');
 
 const clientId = ""
 const redirectUri = "http://localhost/GitHub/akuse/source/"
@@ -23,7 +23,7 @@ const createWindow = () => {
     ipcMain.on("open-login-page", (event) => {
         const authUrl = "https://anilist.co/api/v2/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code"
 
-        win.loadURL(authUrl).then(() => {
+        win.loadURL(authUrl).then(async () => {
             // TEST
             /* const webContents = event.sender
             const win = BrowserWindow.fromWebContents(webContents)
@@ -36,35 +36,32 @@ const createWindow = () => {
             const code = currentUrl.searchParams.get("code")
             /* console.log(code) */
 
-            getAccessToken(code)
+            /* getAccessToken(code) */
+
+            const token = await getToken(code);
+            console.log(token)
         })
     })
 
-    function getAccessToken(code) {
-        var options = {
-            uri: 'https://anilist.co/api/v2/oauth/token',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            json: {
-                'grant_type': 'authorization_code',
-                'client_id': clientId,
-                'client_secret': clientSecret,
-                'redirect_uri': redirectUri,
-                'code': code
-            }
-        }
-        
-        request(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body.access_token)
-            } else {
-                return "Error: " + response.statusCode
-            }
+    async function getToken(code) {
+        const response = await axios({
+          method: "post",
+          url: `https://anilist.co/api/v2/oauth/token`,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          data: {
+            'grant_type': 'authorization_code',
+            'client_id': clientId,
+            'client_secret': clientSecret,
+            'redirect_uri': redirectUri,
+            'code': code
+          }
         })
-    }
+
+        return response.data.access_token
+      }
 }
 
 app.whenReady().then(() => {
