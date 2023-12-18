06b355d5ea90b9bbc2a41967a870bde9b7366d9d
aleganza <ganzarolli.alessio@gmail.com>
2023-12-18 02:37:49 +0100

    US 11061 in animeCustomTitle.js

--- src/modules/animeCustomTitles.js
+++ src/modules/animeCustomTitles.js
@@ -2,17 +2,18 @@
     Some anime are difficult to pull automatically, so the 
     correct titles for those (I hope rare) exceptions are written here.
 
-    AnimeId - AnimeCustomTitle
+    "AnimeId": `AnimeCustomTitle` // problem faced
 */
 
 module.exports = {
     "US": {
-        "113415": `Jujutsu Kaisen Tv`,
-        "150672": `"Oshi No Ko"`
+        "113415": `Jujutsu Kaisen Tv`, // pulled Jujutsu Kaisen 0 movie
+        "150672": `"Oshi No Ko"`, // pulled a total different show
+        "11061": `Hunter X Hunter 2011` // pulled dubbed version
     },
     "IT": {
-        "162670": `Dr. Stone 3 Part 2`,
-        "21778": `Così parlò Kishibe Rohan`,
-        "104578": `L'attacco dei Giganti 3 Parte 2`
+        "162670": `Dr. Stone 3 Part 2`, 
+        "21778": `Così parlò Kishibe Rohan`, // italian title does not exist in anilist
+        "104578": `L'attacco dei Giganti 3 Parte 2` // in anilist the italian title is mispelled (L'Attaco...)
     }
 }
