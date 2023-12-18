4b9a5c8801f7b73f8f7a818b5738afa7ba392b97
aleganza <ganzarolli.alessio@gmail.com>
2023-12-18 13:58:23 +0100

    fix #29
    US 235 animeCustomTitles.js

--- src/modules/animeCustomTitles.js
+++ src/modules/animeCustomTitles.js
@@ -10,7 +10,8 @@ module.exports = {
         "113415": `Jujutsu Kaisen Tv`, // pulled Jujutsu Kaisen 0 movie
         "150672": `"Oshi No Ko"`, // pulled a total different show
         "11061": `Hunter X Hunter 2011`, // pulled dubbed version
-        "43": `Ghost in the Shell` // anilist romaji title is wrong
+        "43": `Ghost in the Shell`, // anilist romaji title is wrong
+        "235": `Detective Conan Remastered` // with this the remastered version is pulled before the default one
     },
     "IT": {
         "162670": `Dr. Stone 3 Part 2`, 
