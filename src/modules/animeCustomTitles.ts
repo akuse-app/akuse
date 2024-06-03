2d91c14173bc9342937fbfe2140b9e6d77ba000f
aleganza <ganzarolli.alessio@gmail.com>
2024-06-03 05:28:24 +0200

    fix: #70

--- src/modules/animeCustomTitles.ts
+++ src/modules/animeCustomTitles.ts
@@ -24,6 +24,7 @@ export const animeCustomTitles: AnimeCustomTitles = {
     '43': { title: `Ghost in the Shell`, index: 0 },
     '235': { title: `Detective Conan Remastered`, index: 0 },
     '1482': { title: `D.Gray-man (2006)`, index: 0 },
+    '132052': { title: 'Kakkou no Iinazuke', index: 1 },
   },
   IT: {
     '11061': { title: `Hunter X Hunter (2011)`, index: 1 },
