5d237451445973c2c23df40ffddad2b28d56ba97
chonker <chonkers.git@gmail.com>
2024-09-06 15:11:25 -0700

    fix: rate limit handler

--- src/modules/requests.ts
+++ src/modules/requests.ts
@@ -1,5 +1,37 @@
 import axios from 'axios';
 
+var remainingRequests = 90;
+var resetTime = 0;
+var lockUntil = 0;
+
+async function delay(seconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+const handleRateLimiting = async (current: number) => {
+  if (current < lockUntil) {
+    await delay(lockUntil - current);
+  }
+
+  if (current >= resetTime) {
+    remainingRequests = 90;
+  }
+
+  if (remainingRequests <= 0) {
+    await delay(60);
+  }
+};
+
+const handleResponseHeaders = (headers: any) => {
+  if (headers['x-ratelimit-remaining']) {
+    remainingRequests = parseInt(headers['x-ratelimit-remaining']);
+  }
+
+  if (headers['x-ratelimit-reset']) {
+    resetTime = parseInt(headers['x-ratelimit-reset']);
+  }
+};
+
 /**
  * Builds the data options for the request
  *
@@ -24,12 +56,43 @@ export const getOptions = (query: any = {}, variables: any = {}) => {
  * @returns object with the fetched data
  * @throws error if the request was not successful
  */
+
 export const makeRequest = async (
   method: 'GET' | 'POST' | string,
   url: string,
   headers: any = {},
   options: any = {},
-) => {
+): Promise<any> => {
+  if (url === 'https://graphql.anilist.co') {
+    const current = Date.now() / 1000;
+
+    await handleRateLimiting(current);
+
+    try {
+      const response = await axios({
+        method: method,
+        url: url,
+        headers: headers,
+        data: options,
+      });
+
+      handleResponseHeaders(response.headers);
+
+      return response.data;
+    } catch (error) {
+      let response = (error as { response?: { status: number, headers: { [key: string]: any } } }).response;
+
+      if (response && response.status === 429) {
+        const retryAfter = parseInt(response.headers['retry-after'] || '60', 10);
+        lockUntil = current + retryAfter;
+        await delay(retryAfter);
+        return makeRequest(method, url, headers, options);
+      }
+
+      throw error;
+    }
+  }
+
   const response = await axios({
     method: method,
     url: url,
