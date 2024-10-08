888b8ae6b8612890874c21135faf41b535f8f75f
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2024-10-08 17:54:43 +0200

    fix: minor design fixes

--- src/renderer/components/AnimeSections.tsx
+++ src/renderer/components/AnimeSections.tsx
@@ -1,6 +1,10 @@
 import './styles/AnimeSection.css';
 
-import { faArrowLeftLong, faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeftLong,
+  faArrowRightLong,
+  faPersonMilitaryToPerson,
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { ListAnimeData } from '../../types/anilistAPITypes';
@@ -21,18 +25,18 @@ interface AnimeSectionsProps {
 }
 
 const filterOptions = (options: Option[]) => {
-  return options.filter(option => option.value.length > 0);
+  return options.filter((option) => option.value.length > 0);
 };
 
 const getSelectedOption = (options: Option[], selectedLabel: string) => {
-  return options.find(option => option.label === selectedLabel) || options[0];
+  return options.find((option) => option.label === selectedLabel) || options[0];
 };
 
 const AnimeSections: React.FC<AnimeSectionsProps> = ({
   options,
   id,
   selectedLabel,
-  onClick
+  onClick,
 }) => {
   const animeListWrapperRef = useRef<HTMLDivElement>(null);
   const animeListRef = useRef<HTMLDivElement>(null);
@@ -42,9 +46,11 @@ const AnimeSections: React.FC<AnimeSectionsProps> = ({
   const filteredOptions = filterOptions(options);
   const selected = useMemo(
     () => getSelectedOption(filteredOptions, selectedLabel),
-    [filteredOptions, selectedLabel]
+    [filteredOptions, selectedLabel],
   );
-  const [animeData, setAnimeData] = useState<ListAnimeData[]>(selected?.value || []);
+  const [animeData, setAnimeData] = useState<ListAnimeData[]>(
+    selected?.value || [],
+  );
 
   useEffect(() => {
     if (selected?.value) {
@@ -86,41 +92,59 @@ const AnimeSections: React.FC<AnimeSectionsProps> = ({
     // console.log(value);
     setAnimeData(value);
     // selectedValue = value;
-  }
+  };
 
   return (
-    <section onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} id={`${id.toLowerCase().replace(' ', '-')}-section`}>
-      <Select
-        options={filteredOptions}
-        selectedValue={animeData}
-        onChange={handleSectionSelect}
-        className={`${id}-select`}
-      />
-      {enableButtons && (
-        <div
-          className={`scrollers ${
-            showButtons ? 'show-opacity' : 'hide-opacity'
-          }`}
-        >
-          <ButtonCircle
-            icon={faArrowLeftLong}
-            tint="dark"
-            small
-            onClick={scrollLeft}
-          />
-          <ButtonCircle
-            icon={faArrowRightLong}
-            tint="dark"
-            small
-            onClick={scrollRight}
-          />
-        </div>
-      )}
+    <section
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      id={`${id.toLowerCase().replace(' ', '-')}-section`}
+    >
+      <div style={{marginTop: 20}}>
+        <Select
+          options={filteredOptions}
+          selectedValue={animeData}
+          onChange={handleSectionSelect}
+          className={`${id}-select`}
+        />
+
+        {enableButtons && (
+          <div
+            className={`scrollers ${
+              showButtons ? 'show-opacity' : 'hide-opacity'
+            }`}
+            style={{ top: 20, right: 25 }}
+          >
+            <ButtonCircle
+              icon={faArrowLeftLong}
+              tint="dark"
+              small
+              onClick={scrollLeft}
+            />
+            <ButtonCircle
+              icon={faArrowRightLong}
+              tint="dark"
+              small
+              onClick={scrollRight}
+            />
+          </div>
+        )}
+      </div>
+      <br />
+
       <div className="anime-list-wrapper" ref={animeListWrapperRef}>
-        <div className="anime-list" ref={animeListRef}>
-        {(animeData ?? Array(20).fill(undefined)).map(
+        <div
+          className="anime-list"
+          style={{ marginRight: 25 }}
+          ref={animeListRef}
+        >
+          {(animeData ?? Array(20).fill(undefined)).map(
             (listAnimeData, index) => (
-              <AnimeEntry onClick={onClick} key={index} listAnimeData={listAnimeData} />
+              <AnimeEntry
+                onClick={onClick}
+                key={index}
+                listAnimeData={listAnimeData}
+              />
             ),
           )}
         </div>
