00df3eec221bd78d768c458959801fed3679790b
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2024-10-15 20:19:34 +0200

    chore: update sponsor url

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,4 +1,4 @@
 
-export const SPONSOR_URL = 'https://github.com/sponsors/aleganza';
+export const SPONSOR_URL = 'https://ko-fi.com/aleganza';
 export const OPEN_NEW_ISSUE_URL = 'https://github.com/aleganza/akuse/issues/new';
 export const EPISODES_INFO_URL = 'https://api.ani.zip/mappings?anilist_id=';
